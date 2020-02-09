f3189c7250524e5516bb5ba757804281b20784bf
Guillaume Legoy <guillaume.legoy@gmail.com>
2020-02-09 22:28:26 +0100

    Added region keywords distribution tables.

---
+++ explorer/dbt/models/keywords.sql
@@ -0,0 +1,7 @@
+{{ config(materialized='table') }}
+
+
+	SELECT DISTINCT
+	    regexp_replace(unnest(string_to_array(keywords, ',')), '[^\w]+','', 'gi') AS keyword
+	FROM
+	    explorer.cards
---
+++ explorer/dbt/models/region_keyword_distributions.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+SELECT
+   k.keyword,
+   c.region,
+   COUNT(*) as region_cards_count,
+   SUM(CASE WHEN c.keywords LIKE '%' || k.keyword || '%' THEN 1 ELSE 0 END) AS keyword_occurence,
+   (SUM(CASE WHEN c.keywords LIKE '%' || k.keyword || '%' THEN 1 ELSE 0 END)::decimal / COUNT(*)::decimal)::decimal(4, 2) AS keyword_perc_of_total_cards
+FROM
+    {{ ref('keywords') }} k
+        CROSS JOIN explorer.cards c
+WHERE
+    keyword <> ''
+GROUP BY 1, 2
