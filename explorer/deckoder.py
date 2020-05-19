01d9e054ed2da945acee7f0a9e391af193f723a1
Guillaume Legoy <guillaume.legoy@gmail.com>
2020-05-19 22:27:06 +0200

    Create first draft of deck code decoder

--- explorer/deckoder.py
+++ explorer/deckoder.py
@@ -1,8 +1,48 @@
-from lor_deckcodes import LoRDeck, CardCodeAndCount
-
+from lor_deckcodes import LoRDeck
+from postgres import Postgres
+import pandas as pd
+import sys
 
 # Decoding
-deck = LoRDeck.from_deckcode('CEBQCAQDA4AQCAY6AMAQCCIUCYCACAQBAEAQEAYBAQAQGCY5GAZAMAIBAQGRKIZNFYAQCAQDAY')
+deck = LoRDeck.from_deckcode(sys.argv[1])
 
 # list all cards with card format 3:01SI001
 print(list(deck))
+
+
+def sql_output_as_dataframe(output) -> list:
+    return [
+        pd.DataFrame(result[0], columns=[x[0] for x in result[1]]) for result in output
+    ]
+
+
+with Postgres("dev/guillaumelegoy") as conn:
+    deck_info = []
+    card_codes = []
+
+    for card in list(deck):
+        card_codes.append(card[-7:])
+
+    card_info = conn.execute_query(
+        f"SELECT card_name, region, cost, attack, health, rarity, main_type, subtype, supertype, keywords "
+        f"FROM legendofruneterra_explorer.cards WHERE code IN %(card_codes)s",
+        parameters={"card_codes": tuple(card_codes)},
+    )
+
+    print(
+        pd.DataFrame(
+            card_info,
+            columns=[
+                "card_name",
+                "region",
+                "cost",
+                "attack",
+                "health",
+                "rarity",
+                "main_type",
+                "subtype",
+                "supertype",
+                "keywords",
+            ],
+        )
+    )
