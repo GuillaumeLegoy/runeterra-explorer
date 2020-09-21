ee2359ee150e8f1a5d8ab74ce7959991b78723ba
Guillaume Legoy <guillaume.legoy@gmail.com>
2020-09-21 18:02:42 +0200

    Reset javascript

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,41 +1,7 @@
 <script>
-  import Search from './search.svelte'
-
-  let searchQuery = ''
-  let searchTerm = null
-
-  function handleSubmit() {
-    searchTerm = searchQuery.trim()
-    searchResults = []
-
-    if (!searchTerm) return
-
-    searchUnsplash()
-  }
-
-  function searchUnsplash() {
-    return 'try again'
-  }
+  import Search from './Search.svelte'
 </script>
 
-<style>
-  .App {
-    width: 100%;
-    max-width: 1500px;
-    padding: 20px;
-    margin: 0 auto 50px;
-    text-align: center;
-  }
-
-  h1 {
-    font-size: 50px;
-    margin-top: 30px;
-    margin-bottom: 30px;
-  }
-</style>
-
-<main class="App">
-  <h1>Deckoder</h1>
-
-  <Search bind:query={searchQuery} {handleSubmit} />
+<main>
+  <Search />
 </main>
---
+++ app/src/Search.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<input
+  type="text"
+  name="searchBar"
+  id="searchBar"
+  placeholder="Enter deck code ..." />
